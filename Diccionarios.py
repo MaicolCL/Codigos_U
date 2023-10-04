b2eb80c245204496fba58f711955f5074be4f7c4
MaicolCL <mcifuentesl@est.ups.edu.ec>
2023-10-04 16:53:14 -0500

    Eliminación de archivos repetidos

--- Diccionarios.py
+++
@@ -1,9 +0,0 @@
-#Funcionamiento de Diccionario
-
-dict1={"Nombre":"Miguel",
-       "ID":"17259642",
-       5623:"Num_emp",
-       "Tel":3654789 ,
-       "email":"mcarrazco@gmial.com",
-       "Tel":365478964
-       }
